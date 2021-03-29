51185bdc35e5d5101880db92a8c8cf99df77b333
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-03-29 13:32:39 +0200

    a

--- include/ph_color/color.hpp
+++ include/ph_color/color.hpp
@@ -1,3 +1,64 @@
+#pragma once
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+namespace _color {
+/**
+ 43
+
+ ANSI escape color codes :
+
+ Name            FG  BG
+ Black           30  40
+ Red             31  41
+ Green           32  42
+ Yellow          33  43
+ Blue            34  44
+ Magenta         35  45
+ Cyan            36  46
+ White           37  47
+ Bright Black    90  100
+ Bright Red      91  101
+ Bright Green    92  102
+ Bright Yellow   93  103
+ Bright Blue     94  104
+ Bright Magenta  95  105
+ Bright Cyan     96  106
+ Bright White    97  107
+ */
+    enum Code {
+        RED      = 91,
+        GREEN    = 92,
+        BLUE     = 34,
+        WHITE  = 97,
+        BLACK = 90,
+        PINK = 95,
+        YELLOW = 93,
+        CYAN = 96,
+//        TEST = 36
+    };
+
+    class Modifier {
+        Code code;
+    public:
+        Modifier(Code pCode) : code(pCode) {}
+        friend std::ostream&
+        operator<<(std::ostream& os, const Modifier& mod) {
+            return os << "\033[" << mod.code << "m";
+        }
+    };
+_color::Modifier red {_color::RED};
+_color::Modifier green {_color::GREEN};
+_color::Modifier blue {_color::BLUE};
+_color::Modifier yellow {_color::YELLOW};
+_color::Modifier black {_color::BLACK};
+_color::Modifier pink {_color::PINK};
+_color::Modifier cyan {_color::CYAN};
+_color::Modifier white {_color::WHITE};
+
+}
+
 template <int code>
 class color : public std::ostream
 {
