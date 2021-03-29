8029bea8709a9483e7844aab0f2839ee49d90cc0
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-03-29 15:18:40 +0200

    a

--- include/ph_color/color.hpp
+++ include/ph_color/color.hpp
@@ -60,8 +60,9 @@ _color::Modifier white {_color::WHITE};
 }
 
 template <int code>
-class color : public std::ostream
+class color : protected std::ostream
 {
+    
     static std::mutex& getCoutMutex()
         // use a Meyers' singleton for the cout mutex to keep this header-only
     {
@@ -72,12 +73,23 @@ class color : public std::ostream
     std::lock_guard<std::mutex> lg_;
 
   public:
+    string str = string ("\033[") + to_string (code) + "m";
     // Generic constructor
     // You need to pass the right mutex to match the stream you want
-    color(std::mutex& m, std::ostream& os)
+    explicit color(std::mutex& m, std::ostream& os)
       : std::ostream(os.rdbuf())
       , lg_{m}
     { }
+    
+    color& operator<<(auto fun) {
+    //    (*fun)(c);
+        cout << fun;
+//        if (fun == endl)
+//        {
+//            cout << "GOTCHA" << endl;
+//        }
+        return *this;
+    }
 
     // Cout specific constructor
     // Uses a mutex singleton specific for cout
@@ -85,6 +97,8 @@ class color : public std::ostream
       : color(getCoutMutex(), std::cout)
     { }
 };
+
+
 
 
 
@@ -98,7 +112,21 @@ color<_color::Code::BLACK> black;
 color<_color::Code::PINK> pink;
 color<_color::Code::CYAN> cyan;
 
+//template <int i>
+//inline ostream& green(ostream& o){
+//    return o;
+//}
 
+//struct _end{};
+//_end nd;
+//
+//template <int code>
+//color<code>& operator<< (color<code>& c, _end& j)
+//{
+//    static_cast<std::ostream&>(c) << "\n";
+//    return c;
+////    return c;
+//}
 
 template <int code, int i>
 color<i>& operator<< (color<code>& c, color<i>& j)
@@ -108,17 +136,139 @@ color<i>& operator<< (color<code>& c, color<i>& j)
 //    return c;
 }
 
-template <int code, typename T>
-color<code>& operator<< (color<code>& c, T const& a)
+//template <int code, typename T>
+//color<code>& operator<< (color<code>& c, T&& a)
+//{
+// 
+//    stringstream ss;
+//    ss << c.str;
+//    ss << forward<T>(a);
+//    static_cast<std::ostream&>(c) << forward<T>(a) << "\033[93m";
+////    c << white;
+//    return c;
+//}
+
+//template <int code>
+//color<code>& operator<< (color<code>& c, ostream&(*f)(ostream&))
+//{
+//    cout << "yoyoyo" << endl;
+//    return c;
+//}
+
+
+
+
+
+
+
+/**
+ cout << left << setw(40) << s
+ <<  setw(30) << s2  << "kuk" << "\t" << endl;
+ */
+
+
+
+
+#include <ostream>
+#include <istream>
+#include <fstream>
+#include <streambuf>
+#include <sstream>
+#include <strstream>
+#include <ios>
+class LoggingOutputStreambuf : public std::streambuf
 {
+    std::streambuf* myDest;
+//    ofstreambuf myLogFile;
+    std::ostream* myOwner;
+protected:
+    int overflow( int ch ) override
+    {
+//        cout << "kndksnf" << endl;
+//        printf(char(ch);
+//        myLogFile.sputc( ch );  //  ignores errors...
+        return myDest->sputc( ch );
+    }
+public:
+    LoggingOutputStreambuf(
+            std::streambuf* dest
+//                           ,
+//            std::string const& logfileName
+                           )
+        : myDest( dest )
+//        , myLogFile( logfileName.c_str(), std::ios_base::out )
+        , myOwner( nullptr )
+    {
+//        if ( !myLogFile.is_open() ) {
+//            //  Some error handling...
+//        }
+    }
+    LoggingOutputStreambuf(
+            std::ostream& dest
+//                           ,
+//            std::string const& logfileName
+                           )
+        : LoggingOutputStreambuf( dest.rdbuf()
+//                                 , logfileName
+                                 )
+    {
+        dest.rdbuf( this );
+        myOwner = &dest;
+    }
+    ~LoggingOutputStreambuf()
+    {
+        cout << "yo" << endl;
+        stringstream buf;
+        streambuf *backup;
+        
+        backup = cout.rdbuf();
+        cout.rdbuf(buf.rdbuf());
+        buf << "normal buf" << endl;
+        cout << "cout to buf" << endl;
+        cout.rdbuf(backup);
+        cout << "normal cout" << endl;
+        cout << buf.str() << endl;
+//        cout << "w" << endl;
+//        std::ostringstream ss;
+//        ss << std::cin.rdbuf();
+//        std::string s = ss.str();
+//        cout << s << endl;
+        if ( myOwner != nullptr ) {
+            myOwner->rdbuf( myDest );
+        }
+    }
+};
 
-    static_cast<std::ostream&>(c) << "\033[" << code << "m" << a;
-//    c << white;
-    return c;
-}
 
-template <int code>
-color<code>& operator<<(color<code>& c, std::ostream& (*fun)(std::ostream&)) {
-    c << "\n";
-    return c;
-}
+
+class MyStream : public std::iostream, private std::streambuf
+{
+private:
+    std::string     str;
+    
+    virtual std::streamsize xsputn(const char* s, std::streamsize n)
+    {
+        cout << "xsputn" << endl;
+        str.append(s, n);
+        setp(&(*str.begin()), &(*str.end()));
+        return n;
+    }
+
+    virtual int overflow(char c)
+    {
+        str.push_back(c);
+        setp(&(*str.begin()), &(*str.end()));
+        return c;
+    }
+
+
+public:
+    MyStream() : std::iostream(this), str() { }
+    ~MyStream() {
+        
+    }
+    void print()
+    {
+        std::cout << str;
+    }
+};
