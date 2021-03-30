e1545c8e8d17424c4039ed4224f34633cb3c5c2a
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-03-30 13:57:03 +0200

    a

--- include/ph_color/color.hpp
+++ include/ph_color/color.hpp
@@ -27,6 +27,15 @@ namespace _color {
  Bright Cyan     96  106
  Bright White    97  107
  */
+#define yellow "\033[93m"
+#define green "\033[92m"
+#define black "\033[90m"
+#define red "\033[91m"
+#define blue "\033[94m"
+#define magenta "\033[95m"
+#define cyan "\033[96m"
+#define white "\033[97m"
+
     enum Code {
         RED      = 91,
         GREEN    = 92,
@@ -48,69 +57,32 @@ namespace _color {
             return os << "\033[" << mod.code << "m";
         }
     };
-_color::Modifier red {_color::RED};
-_color::Modifier green {_color::GREEN};
-_color::Modifier blue {_color::BLUE};
-_color::Modifier yellow {_color::YELLOW};
-_color::Modifier black {_color::BLACK};
-_color::Modifier pink {_color::PINK};
-_color::Modifier cyan {_color::CYAN};
-_color::Modifier white {_color::WHITE};
+//_color::Modifier red {_color::RED};
+//_color::Modifier green {_color::GREEN};
+//_color::Modifier blue {_color::BLUE};
+//_color::Modifier yellow {_color::YELLOW};
+//_color::Modifier black {_color::BLACK};
+//_color::Modifier pink {_color::PINK};
+//_color::Modifier cyan {_color::CYAN};
+//_color::Modifier white {_color::WHITE};
 
 }
 
-template <int code>
-class color : protected std::ostream
-{
-    
-    static std::mutex& getCoutMutex()
-        // use a Meyers' singleton for the cout mutex to keep this header-only
-    {
-        static std::mutex m;
-        return m;
-    }
-
-    std::lock_guard<std::mutex> lg_;
-
-  public:
-    string str = string ("\033[") + to_string (code) + "m";
-    // Generic constructor
-    // You need to pass the right mutex to match the stream you want
-    explicit color(std::mutex& m, std::ostream& os)
-      : std::ostream(os.rdbuf())
-      , lg_{m}
-    { }
-    
-    color& operator<<(auto fun) {
-    //    (*fun)(c);
-        cout << fun;
-//        if (fun == endl)
-//        {
-//            cout << "GOTCHA" << endl;
-//        }
-        return *this;
-    }
-
-    // Cout specific constructor
-    // Uses a mutex singleton specific for cout
-    color()
-      : color(getCoutMutex(), std::cout)
-    { }
-};
 
 
 
 
 
 
-color<_color::Code::RED> red;
-color<_color::Code::WHITE> white;
-color<_color::Code::GREEN> green;
-color<_color::Code::BLUE> clue;
-color<_color::Code::YELLOW> yellow;
-color<_color::Code::BLACK> black;
-color<_color::Code::PINK> pink;
-color<_color::Code::CYAN> cyan;
+
+//color<_color::Code::RED> red;
+//color<_color::Code::WHITE> white;
+//color<_color::Code::GREEN> green;
+//color<_color::Code::BLUE> clue;
+//color<_color::Code::YELLOW> yellow;
+//color<_color::Code::BLACK> black;
+//color<_color::Code::PINK> pink;
+//color<_color::Code::CYAN> cyan;
 
 //template <int i>
 //inline ostream& green(ostream& o){
@@ -128,13 +100,13 @@ color<_color::Code::CYAN> cyan;
 ////    return c;
 //}
 
-template <int code, int i>
-color<i>& operator<< (color<code>& c, color<i>& j)
-{
-    static_cast<std::ostream&>(c) << "\033[" << i << "m";
-    return j;
-//    return c;
-}
+//template <int code, int i>
+//color<i>& operator<< (color<code>& c, color<i>& j)
+//{
+//    static_cast<std::ostream&>(c) << "\033[" << i << "m";
+//    return j;
+////    return c;
+//}
 
 //template <int code, typename T>
 //color<code>& operator<< (color<code>& c, T&& a)
@@ -176,7 +148,7 @@ color<i>& operator<< (color<code>& c, color<i>& j)
 #include <sstream>
 #include <strstream>
 #include <ios>
-class LoggingOutputStreambuf : public std::streambuf
+class LoggingOutputStreambuf : public std::streambuf, public ostream
 {
     std::streambuf* myDest;
 //    ofstreambuf myLogFile;
@@ -208,26 +180,30 @@ public:
 //                           ,
 //            std::string const& logfileName
                            )
-        : LoggingOutputStreambuf( dest.rdbuf()
-//                                 , logfileName
-                                 )
+        : myDest( dest.rdbuf()), myOwner (nullptr), ostream(this)
     {
         dest.rdbuf( this );
         myOwner = &dest;
+//        myDest->sputc('\0');
+//        myDest->sputc('3');
+//        myDest->sputc('[');
+//        myDest->sputc('9');
+//        myDest->sputc('3');
+//        myDest->sputc('m');
     }
     ~LoggingOutputStreambuf()
     {
-        cout << "yo" << endl;
-        stringstream buf;
-        streambuf *backup;
-        
-        backup = cout.rdbuf();
-        cout.rdbuf(buf.rdbuf());
-        buf << "normal buf" << endl;
-        cout << "cout to buf" << endl;
-        cout.rdbuf(backup);
-        cout << "normal cout" << endl;
-        cout << buf.str() << endl;
+//        cout << "yo" << endl;
+//        stringstream buf;
+//        streambuf *backup;
+//        
+//        backup = cout.rdbuf();
+//        cout.rdbuf(buf.rdbuf());
+//        buf << "normal buf" << endl;
+//        cout << "cout to buf" << endl;
+//        cout.rdbuf(backup);
+//        cout << "normal cout" << endl;
+//        cout << buf.str() << endl;
 //        cout << "w" << endl;
 //        std::ostringstream ss;
 //        ss << std::cin.rdbuf();
@@ -272,3 +248,40 @@ public:
         std::cout << str;
     }
 };
+
+
+
+
+
+
+
+
+namespace ansi {
+  template < class CharT, class Traits >
+  constexpr
+  std::basic_ostream< CharT, Traits > & reset( std::basic_ostream< CharT, Traits > &os )
+  {
+     return os << "\033[0m";
+  }
+
+  template < class CharT, class Traits >
+  constexpr
+  std::basic_ostream< CharT, Traits > & foreground_black( std::basic_ostream< CharT, Traits > &os )
+  {
+     return os << "\033[30m";
+  }
+
+  template < class CharT, class Traits >
+  constexpr
+  std::basic_ostream< CharT, Traits > & foreground_red( std::basic_ostream< CharT, Traits > &os )
+  {
+     return os << "\033[31m";
+  }
+
+ } // ansi
+
+
+/**
+ std::cout << ansi::foreground_red << "in red" << ansi::reset << std::endl;
+
+ */
